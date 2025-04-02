2c0de480d492a2df6feff4aa592f6e6d3b4d19f8
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-04-02 18:18:23 -0300

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,5 @@
+salario = float(input("Informe seu salário atual: R$ "))
+
+novo_salario = salario * 1.10  # Aumento de 10%
+
+print(f"\nSeu novo salário com 10% de aumento será: R$ {novo_salario:.2f}")
