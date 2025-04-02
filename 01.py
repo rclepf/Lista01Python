b6ab196c3413286c93c1cb1d5b04718df19027b8
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-04-02 18:17:47 -0300

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,5 @@
+nome = input("Informe seu nome: ")
+idade = input("Informe sua idade: ")
+cidade = input("Informe sua cidade: ")
+
+print(f"\nOlá, {nome}! Você tem {idade} anos e mora em {cidade}.")
