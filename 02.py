463ad4fecac3247915644b41411bd4e351c41484
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-04-02 18:18:05 -0300

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,9 @@
+num1 = float(input("Informe o primeiro número: "))
+num2 = float(input("Informe o segundo número: "))
+num3 = float(input("Informe o terceiro número: "))
+
+soma = num1 + num2 + num3
+media = soma / 3
+
+print(f"\nSoma: {soma}")
+print(f"Média: {media:.2f}")
